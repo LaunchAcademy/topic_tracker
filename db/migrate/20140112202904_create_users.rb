ce11add70563c89b5f72cac42f8483c5dd429747
Austin Winslow <awinslow21@gmail.com>
2014-01-13 14:02:32 -0500

    weird formatting in first migration

--- db/migrate/20140112202904_create_users.rb
+++ db/migrate/20140112202904_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration
     t.string :display_name
     t.timestamps
   end
-  
+
   add_index "users", ["github_uid"], name: "index_users_on_github_uid", unique: true, using: :btree
   end
 end
