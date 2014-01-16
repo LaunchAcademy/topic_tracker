4bf549eaecd143ef32d6eb33879bb462a8095efa
Eric Kelly <heroiceric@gmail.com>
2014-01-16 14:40:13 -0500

    Add ember appkit rails

---
+++ config/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require environment
+//= require ember-appkit
+//= require_self
+//= require adapter
+//= require router
+//= require_tree ../app
+//= require_tree ./initializers
+
+window.App = require('app').default.create();
